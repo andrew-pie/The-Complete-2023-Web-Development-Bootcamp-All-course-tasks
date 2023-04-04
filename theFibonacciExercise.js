0dadc0ed7e83aa53c3bb8510eae5790efceed33e
Andrii Pyrozhok <pyrozhok.dev@gmail.com>
2023-04-04 09:27:27 +0700

    start

---
+++ theFibonacciExercise.js
@@ -0,0 +1,19 @@
+function fibonacciGenerator (n) {
+	//Do NOT change any of the code above 👆
+		 
+		 //Write your code here:
+		 var output = [];
+		 var fNum = -1;
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 //Return an array of fibonacci numbers starting from 0.
+		 
+	//Do NOT change any of the code below 👇
+	}
+	
+	
