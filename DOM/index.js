af1e6a87c32966060458b71f983317a3179e5c04
Andrii Pyrozhok <pyrozhok.dev@gmail.com>
2023-04-06 10:34:48 +0700

    dice site

--- DOM/index.js
+++ DOM/index.js
@@ -1,2 +1,6 @@
 alert("hello");
 document.querySelector("li a").style.color = "red";
+document.querySelector("button").style.backgroundColor = "yellow";
+document.querySelector("h1").classList.add("huge");
+document.querySelector("h1").textContent;
+document.querySelector("h1").innerHTML = "<em>Good Bye</em>";
---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,25 @@
+const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber1);
+
+const randomDiceImg = "dice" + randomNumber1 + ".png";
+
+const randomImageSource = "images/" + randomDiceImg;
+
+const image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+if (randomNumber1 > randomNumber2) {
+	document.querySelector("h1").innerHTML = "Play 1 wins!";
+} else if (randomNumber2 > randomNumber1) {
+	document.querySelector("h1").innerHTML = "Play 2 wins!";
+} else {
+	document.querySelector("h1").innerHTML = "Draw!";
+}
